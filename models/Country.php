def55102c75a7d22f7638507c48d3127f2c12ab0
Loveorigami <loveorigami@mail.ru>
2016-08-17 18:52:21 +0300

    del constraint trait

--- models/Country.php
+++ models/Country.php
@@ -15,15 +15,10 @@ use Yii;
  */
 class Country extends \lo\core\db\ActiveRecord
 {
-
-    use \lo\core\rbac\ConstraintTrait;
-
     const STATUS_DRAFT = 0;
     const STATUS_PUBLISHED = 1;
 
     const DEF_COUNTRY = 188;
-
-    public $tplDir = '@lo/modules/geo/modules/admin/views/country/tpl/';
 
     /**
      * @inheritdoc
